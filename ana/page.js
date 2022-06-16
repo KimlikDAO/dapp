13d37d7a4e96af84a68e873e61d9b1658d5fdb31
Muhammet <mcoskun86@gmail.com>
2022-06-16 16:54:15 +0300

    Add non functional dropdown button to ana page

--- ana/page.js
+++ ana/page.js
@@ -1,6 +1,31 @@
 // Adımlar
-const a = document.getElementById('a')
+const a = document.getElementById("a");
 // Banner
-const b = document.getElementById('b')
+const b = document.getElementById("b");
 // Banner, açiklama
-const ba = document.getElementById('ba')
+const ba = document.getElementById("ba");
+
+//Dropdown
+function showDropdown() {
+  document.getElementById("ld-dropdown-content").classList.toggle("show");
+}
+function setLanguageTurkish() {
+  document.getElementById("ld-dropdown-button").innerHTML = "Türkçe";
+}
+function setLanguageEnglish() {
+  document.getElementById("ld-dropdown-button").innerHTML = "English";
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".ld-dropbtn")) {
+    var dropdowns = document.getElementsByClassName("ld-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
