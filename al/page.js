f149c8fc52b1a9a077ad1275ea34390757a0f6ff
Muhammet Coşkun <mcoskun86@gmail.com>
2022-06-30 15:05:01 +0300

    al page social setup box

--- al/page.js
+++ al/page.js
@@ -8,6 +8,7 @@ const s1b = Adıyla("s1b");
 const s2a = Adıyla("s2a");
 const s3a = Adıyla("s3a");
 const s4a = Adıyla("s4a");
+const s5a = Adıyla("s5a");
 
 let Hesap = null;
 let Rand = new Uint8Array(20);
@@ -118,16 +119,97 @@ async function açikAnahtarAl() {
   s3a.onclick = null;
   s3a.classList.add("disabled");
   Adıyla("s3").classList.add("done");
+  dördüncüAdımHazırla()
+  sonAdımHazırla() 
+}
 
-  sonAdımHazırla();
+async function dördüncüAdımHazırla() {
+  Adıyla("s4").classList.remove("disabled")
+  s4a.onclick= socialRevokeEkle;
+  Adıyla("s4b").onclick= socialRevokeEkleme;
+  console.log("4.adım")
+}
+
+dördüncüAdımHazırla()
+
+async function socialRevokeEkle() {
+  Adıyla("social-revoke-form").classList.remove("invisible")
+  Adıyla("s4c").onclick= inputFieldEkle;
+  Adıyla("s4d").onclick= socialEkleOnay;
+  Adıyla("s4e").onclick= socialEkleIptal;
+  console.log("clicked evet")
+}
+
+async function socialRevokeEkleme() {
+  Adıyla("s4").classList.add("done");
+  s4a.onclick= null;
+  s4a.innerHTML = "Social Revoke Eklenmedi"
+  Adıyla("s4b").style.display= "none";
+  sonAdımHazırla()
+}
+
+async function inputFieldEkle() {
+  const div = document.createElement("div")
+  const input1 = document.createElement("input")
+  const input2 = document.createElement("input")
+  div.classList.add("container")
+  input1.classList.add("address-input")
+  input1.type= "text"
+  input1.setAttribute("onblur", "adresGecerliMi(this.value)")
+  input2.classList.add("weight-input")
+  input2.type= "number"
+  input2.onblur= agırlıkHesapla
+  input2.value= 1
+  div.appendChild(input1)
+  div.appendChild(input2)
+  Adıyla("social-revoke-S").insertBefore(div,Adıyla("br"))
+  agırlıkHesapla()
+}
+
+async function socialEkleOnay() {
+  console.log('clicked Tamam');
+  const nodeList = [...document.querySelectorAll(".container")] /* adıyla */
+  const addressList = nodeList.map(element => element.children[0].value)
+  const weightList = nodeList.map(element => element.children[1].value)
+  const threshold = Adıyla("threshold").value;
+  const totalWeight = Adıyla("total").value;
+  console.log(addressList, weightList);
+  console.log(threshold, totalWeight);
+  Adıyla("s4").classList.add("done");
+  Adıyla("social-revoke-form").classList.add("invisible");
+  s4a.onclick= null;
+  s4a.innerHTML = "Social Revoke Eklendi"
+  Adıyla("s4b").style.display= "none";
+  sonAdımHazırla()
+}
+
+async function adresGecerliMi(address) {
+  if (address.length != 42 || !address.startsWith("0x")) {
+    return false
+  }
+  return true
+}
+
+async function agırlıkHesapla() {
+  var total = 0;
+  const nodeList = [...document.querySelectorAll(".container")]; /* adıyla */
+  const totalWeightList = nodeList.map(element => Number(element.children[1].value));
+  for ( var i in totalWeightList) {
+    total += totalWeightList[i]
+  }
+  Adıyla("total").value = total;
+}
+
+async function socialEkleIptal() {
+  Adıyla("social-revoke-form").classList.add("invisible")
+  console.log('clicked İptal')
 }
 
 async function sonAdımHazırla() {
-  Adıyla("s4").classList.remove("disabled");
-  s4a.onlick = öde();
-  s4a.classList.remove("disabled");
+  Adıyla("s5").classList.remove("disabled");
+  s5a.onlick = öde();
+  s5a.classList.remove("disabled");
 }
-
 async function öde() {}
 
 açıkTCKTÇek();
