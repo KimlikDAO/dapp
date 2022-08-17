26434996e95fdf71d02034127bb5277ab59e193a
Muhammet Coşkun <mcoskun86@gmail.com>
2022-08-17 14:05:06 +0300

    💄 Oy kartına acılma özelliği ekle

--- oyla/sayfa.js
+++ oyla/sayfa.js
@@ -3,3 +3,15 @@
  *
  */
 import '/birim/cüzdan/birim';
+import dom from '/lib/dom';
+
+dom.adla("oya0").onclick = () => {
+  dom.adla("oya0").classList.add("expand");
+  dom.adla("oya0tb").style.display = "";
+}
+
+dom.adla("oya0tb").onclick = (e) => {
+  dom.adla("oya0").classList.remove("expand");
+  dom.adla("oya0tb").style.display = "none";
+  e.stopPropagation();
+}
