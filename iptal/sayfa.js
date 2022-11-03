fd3f43ad384eadf7cf1e867bb2f3cca78ec9d1df
Muhammet Coşkun <mcoskun86@gmail.com>
2022-11-03 12:13:34 +0300

    ❌ Iptal sayfası timeout ekle ve hata mesajı olustur

--- iptal/sayfa.js
+++ iptal/sayfa.js
@@ -5,7 +5,12 @@ import Cüzdan from '/birim/cüzdan/birim';
 import TCKT from '/lib/ethereum/TCKT';
 import dom from '/lib/util/dom';
 
+Cüzdan.adresDeğişince(() => location.reload());
 dom.adla("ipbtnb").onclick = () => {
+  revokeeAdımınıGöster();
+  Cüzdan.ağDeğişince(revokeeAdımınıGöster);
+}
+const revokeeAdımınıGöster = () => {
   /** @type {?Element} */
   let seçilmişAdres = null;
   dom.adla("ipssc").classList.add("done");
@@ -14,32 +19,78 @@ dom.adla("ipbtnb").onclick = () => {
   dom.adlaGizle("iptac");
   dom.adlaGöster("ipiic");
   const onaylaDüğmesi = dom.adla("ipiio");
-  onaylaDüğmesi.classList.add("act");
-  onaylaDüğmesi.classList.remove("dis");
-  onaylaDüğmesi.innerText = dom.TR ? "Onayla" : "Confirm";
-  let innerHTML = "";
-  const ul = dom.adla("ipiil");
-  dom.adlaGöster("iplc"); // Yükleniyor animasyonu
-  TCKT.getRevokeeAddresses(/** @type {string} */(Cüzdan.adres())).then((data) => {
-    const filtered = data.map((element) => "0x" + element.topics[1].slice(26));
-    if (filtered.length == 0) {
-      innerHTML = dom.TR
-        ? "İptal edebileceğiniz bir adres yok."
-        : "There is no revoke address."
-    }
-    for (let i = 0; i < filtered.length; ++i) {
-      innerHTML += `<li id=ipiia${i} class="ipiia">${filtered[i]}</li>`
-    }
-    ul.onclick = (e) => {
-      let li = e.target;
-      if (seçilmişAdres) seçilmişAdres.classList.remove("sel");
-      li.classList.add("sel");
-      seçilmişAdres = li;
-    }
-    ul.innerHTML = innerHTML;
-    dom.adlaGöster("ipiilc");
-    dom.adlaGizle("iplc");
-  })
+
+  const onaylaDüğmesiDüzelt = () => {
+    onaylaDüğmesi.classList.add("act");
+    onaylaDüğmesi.classList.remove("dis");
+    onaylaDüğmesi.innerText = dom.TR ? "Onayla" : "Confirm";
+  }
+
+  onaylaDüğmesiDüzelt();
+
+  const hataMesajınıKapat = () => {
+    dom.adlaGizle("ipmc");
+    dom.adlaGizle("iphm");
+    dom.adlaGizle("ipaym");
+  }
+
+  const revokeeAdresAra = () => {
+    dom.adlaGizle("ipiilc");
+    hataMesajınıKapat();
+    let innerHTML = "";
+    const ul = dom.adla("ipiil");
+    dom.adlaGöster("iplc"); // Yükleniyor animasyonu
+    const timer = setTimeout(() => {
+      dom.adlaGizle("iplc");
+      dom.adlaGöster("ipmc");
+      dom.adla("iphm").innerText = dom.TR ?
+        "İstek zaman aşımına uğradı." :
+        "Request timed out.";
+      dom.adlaGöster("iphm");
+      onaylaDüğmesi.innerText = dom.TR ? "Tekrar Dene" : "Retry";
+      onaylaDüğmesi.onclick = () => {
+        clearTimeout(timer);
+        revokeeAdımınıGöster();
+      }
+    }, 2000)
+    TCKT.getRevokeeAddresses(/** @type {string} */(Cüzdan.adres())).then((data) => {
+      const filtered = data.map((element) => "0x" + element.topics[1].slice(26));
+      clearTimeout(timer);
+      if (filtered.length == 0) {
+        dom.adlaGöster("ipmc");
+        dom.adlaGöster("ipaym");
+        dom.adlaGizle("iplc");
+        dom.adlaGizle("iphm");
+        dom.butonDurdur(onaylaDüğmesi);
+        onaylaDüğmesi.classList.remove("act");
+      } else {
+        for (let i = 0; i < filtered.length; ++i) {
+          innerHTML += `<li id=ipiia${i} class="ipiia">${filtered[i]}</li>`
+        }
+        ul.onclick = (e) => {
+          let li = e.target;
+          if (seçilmişAdres) seçilmişAdres.classList.remove("sel");
+          li.classList.add("sel");
+          seçilmişAdres = li;
+        }
+        ul.innerHTML = innerHTML;
+        dom.adlaGöster("ipiilc");
+        dom.adlaGizle("iplc");
+        hataMesajınıKapat();
+        onaylaDüğmesiDüzelt();
+      }
+    }).catch((e) => {
+      clearTimeout(timer);
+      dom.adlaGöster("ipmc");
+      dom.adlaGöster("iphm");
+      onaylaDüğmesi.innerText = dom.TR ? "Tekrar Dene" : "Retry";
+      onaylaDüğmesi.onclick = () => {
+        revokeeAdımınıGöster();
+      }
+    })
+  }
+
+  revokeeAdresAra();
 
   onaylaDüğmesi.onclick = () => {
     if (seçilmişAdres) {
