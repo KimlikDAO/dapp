1184e6f889632fb392cbf938cc49a5219f7fc68c
Muhammet Coşkun <mcoskun86@gmail.com>
2022-11-06 16:11:39 +0300

    ..

--- oduller/sayfa.js
+++ oduller/sayfa.js
@@ -4,65 +4,24 @@
 
 import Cüzdan from '/birim/cüzdan/birim';
 import dom from '/lib/util/dom';
+import TCKT from '/lib/ethereum/TCKT';
+// import SDK from '/sdk/SDK'; 
 
-/** @type {Object<string, string>} */
-let data = {};
-/** @type {string} */
-let humanId = "";
-/** @type {string} */
-let eposta = "";
-/** @type {string} */
-let telefon = "";
-
-dom.adla("ods0o").onclick = () => dom.adla("na").click();
-Cüzdan.bağlanınca(() => {
-  idAdımınıGöster();
-})
-
-const idAdımınıGöster = () => {
+dom.adla("ods0o").onclick = Cüzdan.bağla();
+Cüzdan.bağlanınca((adres) => {
   dom.adlaGizle("ods0c");
   dom.adlaGöster("ods1c");
-  dom.adla("odhidi").onfocus = () => dom.adla("odhidi").classList.remove("odin");
-  dom.adla("ods1o").onclick = () => {
-    humanId = dom.adla("odhidi").value;
-    if (humanId.length !== 8) {
-      dom.adla("odhidi").classList.add("odin");
-      return
-    }
-    data["humanId"] = humanId;
-    console.log(data);
-    contactInfoAdımınıGöster();
-  }
+  tcktKontrolEt(adres);
+});
+
+/**
+ * @param {string} adres
+ */
+const tcktKontrolEt = (adres) => {
+  // const handle = SDK.getTCKTHandle(adres);
+  // if (!handle) {
+
+  // }
 }
 
-const contactInfoAdımınıGöster = () => {
-  dom.adla("ods1c").classList.add("done");
-  dom.butonDurdur(dom.adla("ods1o"));
-  dom.adla("odhidi").disabled = true;
-  dom.adla("ods1o").innerText = dom.TR ? "ID'nizi aldık ✓" : "We got your ID ✓";
-  dom.adlaGöster("ods2c");
-  dom.adla("odemi").onfocus = () => dom.adla("odemi").classList.remove("odin");
-  dom.adla("odti").onfocus = () => dom.adla("odti").classList.remove("odin");
-  dom.adla("odti").onkeydown = () => {
-    if (dom.adla("odti").value.length == 3 || dom.adla("odti").value.length == 7) {
-      dom.adla("odti").value += " ";
-    }
-  }
-  dom.adla("ods2o").onclick = () => {
-    eposta = dom.adla("odemi").value;
-    telefon = dom.adla("odti").value;
-    if (!eposta.includes("@")) {
-      dom.adla("odemi").classList.add("odin");
-      return
-    }
-    if (telefon.length != 12) {
-      dom.adla("odti").classList.add("odin");
-      return
-    }
-    console.log(eposta);
-    console.log(telefon);
-    data["eposta"] = eposta;
-    data["telefon"] = telefon;
-    console.log(data);
-  }
-}
+
