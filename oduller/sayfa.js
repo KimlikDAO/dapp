338bde7a6c692e202f6c80f41d7b80d3a2714bc4
Muhammet Coşkun <mcoskun86@gmail.com>
2022-11-21 16:57:34 +0300

    Transfer gecmisi ui degistir

--- oduller/sayfa.js
+++ oduller/sayfa.js
@@ -5,23 +5,44 @@
 import Cüzdan from '/birim/cüzdan/birim';
 import dom from '/lib/util/dom';
 import TCKT from '/lib/ethereum/TCKT';
-// import SDK from '/sdk/SDK'; 
+// import SDK from '/sdk/src'; 
 
-dom.adla("ods0o").onclick = Cüzdan.bağla();
+/** @const {Element} */
+const cüzdanBağlaDüğmesi = dom.adla("ods0b");
+
+cüzdanBağlaDüğmesi.onclick = Cüzdan.bağla();
 Cüzdan.bağlanınca((adres) => {
-  dom.adlaGizle("ods0c");
-  dom.adlaGöster("ods1c");
-  tcktKontrolEt(adres);
+  dom.adla("ods0c").classList.add("done");
+  dom.butonDurdur(cüzdanBağlaDüğmesi);
+  cüzdanBağlaDüğmesi.classList.remove("act");
+  cüzdanBağlaDüğmesi.innerText = dom.TR ? "Cüzdan bağlandı ✓" : "Wallet connected ✓";
+  tcktKontrolEt(adres, Cüzdan.ağ());
 });
 
 /**
  * @param {string} adres
+ * @param {string} adres
  */
-const tcktKontrolEt = (adres) => {
-  // const handle = SDK.getTCKTHandle(adres);
-  // if (!handle) {
-
-  // }
+const tcktKontrolEt = (adres, chainId) => {
+  if (true) { // if (SDK.doesOwnTCKT(adres, chainId)) 
+    dom.adlaGöster("ods1ac");
+    dom.adla("ods1ab").onclick = () => { // TCKT humanId ve contactInfo 
+      bilgileriKontrolEt();
+    }
+  } else { 
+    dom.adlaGöster("ods1bc");
+  }
 }
 
-
+const bilgileriKontrolEt = () => {
+  dom.adla("ods1ac").classList.add("done");
+  dom.butonDurdur(dom.adla("ods1ab"));
+  dom.adla("ods1ab").classList.remove("act");
+  dom.adla("ods1ab").innerText = dom.TR ? "Bilgileriniz alındı ✓" : "We got your info ✓";
+  if (true) {   // Kampanyaya kaydı kontrol edilecek
+    dom.adlaGöster("ods2ac");
+    dom.adla("odtx").innerText = "Transaction"; //tx gelecek
+  } else {
+    dom.adlaGöster("ods2bc");
+  }
+}
